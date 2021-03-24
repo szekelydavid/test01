5cb398298da56ed94e9426733a15c66ead92db05
zekely David Attila <szekelydavidattila@gmail.com>
2021-03-24 13:13:10 +0100

    *

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,19 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        number=Math.abs(number);
+        if((number%3==0)&&(number%7==0)){
+            String returnConcatString="";
+            returnConcatString +=words[0];
+            returnConcatString +=words[1];
+            return returnConcatString;
+        }
+        if(number%3==0){
+            return words[0];
+        }
+        if(number%7==0){
+            return words[1];
+        }
+        return "";
     }
 }
